691f74af8226cb9454a908f12ecc215f72140105
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-08 18:28:02 +0900

    Update TRY_2_42p_.go

--- TRY_2_42p_.go
+++ TRY_2_42p_.go
@@ -5,8 +5,23 @@ import ("fmt"
 "time"
 )
 
-// 2章 基本構文 42p 【TRY】 おみくじプログラムを作ろう 
-// 正答例リンク""
+func main() {
+// 2章 基本構文 41p 【TRY】 奇数と偶数 
+// 正答例リンク"https://play.golang.org/p/pruHZLxX8jc"
+	t := time.Now().UnixNano() //現在時刻を数値で取得
+	rand.Seed(t) //乱数の種を設定？ ←要復習
+	n := rand.Intn(6) // 0-5までの乱数を発生
+// println(s) //←多分、下にprintlnあるから要らない。
 
-
-
+		switch n {
+		case i == 5: // iが5の場合 
+			fmt.Println("大吉")
+		case i == 4,3: // iが4か3の場合  
+			fmt.Println("中吉")
+		case i == 2,1: // iが2か1の場合 
+			fmt.Println("吉")
+		default: // そうでない場合（数値は0~5の乱数なので0しかない）
+			fmt.Println("凶")
+		}
+	}
+}
