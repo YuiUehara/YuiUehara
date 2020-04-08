c3604884e9ec80758a5a2a0198a83e466c8053c9
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-08 14:48:30 +0900

    Update TRY_2_41p_.go

--- TRY_2_41p_.go
+++ TRY_2_41p_.go
@@ -7,13 +7,25 @@ func main() {
 // 2章 基本構文 41p 【TRY】 奇数と偶数 
 // 正答例リンク"https://play.golang.org/p/pruHZLxX8jc"
 	for i := 1; i <= 100; i++ { /* 100まで繰り返せ */
-			if i%2 == 0 {/* もしもiが偶数（2で割った数が0）だったら */
-/*				fmt.Println(i + "-偶数")*/
+			if i%2 == 0 {// もしもiが偶数（2で割った数が0）だったら 
+//				fmt.Println(i + "-偶数")
 				fmt.Printf("%d-偶数\n", i)
-				/* %d は整数を10進法で出力するの意の指定子 */
+				// %d は整数を10進法で出力するの意の指定子
 			} else {
-/*				fmt.Println(i + "-奇数")*/
+//				fmt.Println(i + "-奇数")
 				fmt.Printf("%d-奇数\n", i)
 			}
 		}
+	for i := 1; i <= 100; i++ {
+			switch {
+			case i%2 == 0: // iが偶数（2で割った数が0）の場合は 
+				fmt.Printf("%d-偶数\n", i)
+				break;
+			default: // そうでない場合は
+				fmt.Printf("%d-奇数\n", i)
+				break;
+			}
+		}
 }
+// if~else文とswitch case defaultの違いとは？
+// 二分岐だとif文、多分岐だとswitchが適切らしい。
