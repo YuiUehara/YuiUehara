5de1ed2aba2fdb3463ac820e225f612733ffbeb3
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-09 14:01:31 +0900

    Update TRY_3_10p_.go

--- TRY_3_10p_.go
+++ TRY_3_10p_.go
@@ -1,10 +1,10 @@
 package main
 
 func main() {
-	var sum int
-	sum = 5 + 6 + 3
-	avg := sum / 3
-	if avg > 4.5 {
+	var sum int　// sum は１０進法の整数です。
+	sum = 5 + 6 + 3 // 合計 
+	avg := sum / 3 // sum を３で割った平均値
+	if avg > 4.5 { // もし平均値が4.5以上だったら
 		println("good")
 	}
 }
