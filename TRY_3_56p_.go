2de439444f319165e9cc9a855911a971f5fe2f70
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-10 16:03:49 +0900

    Update TRY_3_56p_.go

--- TRY_3_56p_.go
+++ TRY_3_56p_.go
@@ -3,12 +3,16 @@ package main
 func main() {
 	for i := 1; 1 <= 100; i++{
 		print(i)
-		if i%2 == 0 {
+		if(i%2 == 0 && i != 100) {
 			println("-偶数")
-		} else {
+		} else if(i%2 == 1) {
 			println("-奇数")
+		} else {
+			println("-偶数")
+			break;
 		}
 	}
 }
+	
 
-// 
+// 何で１００まで繰り返さないのかわからないけど、とりあえず100でカウントが止まるようにした。
