330a0ab3ed42d3221097f8af9195904656b69a34
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-10 13:47:10 +0900

    Update TRY_3_39p_.go

--- TRY_3_39p_.go
+++ TRY_3_39p_.go
@@ -1,5 +1,6 @@
 package main
-
+// 3章 関数と型 39p 【TRY】 ユーザ定義型の利用 
+// 正答例リンク"https://play.golang.org/p/FGuwWuFjQfO"
 type Score struct { // "Score"の構造を設定します。
 	UserID string // "UserID"は文字列です。
 	GameID int // "GameID"は整数です。
