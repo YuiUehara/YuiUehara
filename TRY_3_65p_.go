3e2aaaba44ae7953968f69a5e0779feff07e35f7
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-14 08:08:03 +0900

    Create TRY_3_65p_.go

---
+++ TRY_3_65p_.go
@@ -0,0 +1,22 @@
+package main
+// 3章 関数と型 65p 【TRY】 レシーバに変更を加える
+
+type MyInt int
+// Incメソッドは自身を1ずつ加算する
+
+func (n *MyInt) Int() { *n++ } //←こいつが1ずつ加算
+
+func main() {
+	var n MyInt // 
+	println(n) // 
+//	(n).Inc() // 
+	(&n).Inc() // 
+	println(n) // 
+}
+
+
+// n.Inc undefined (type *MyInt has no field or method Inc)
+// *MyInt にフィールドかメソッドがないよ
+// あるやんけ
+
+// レシーバに変更を加えるのはポインタ型
