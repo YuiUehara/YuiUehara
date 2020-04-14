49218bfd59fc8fed4ccba7365e3f7eb5d1d102ae
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-14 13:26:23 +0900

    Update TRY_3_56p_.go

--- TRY_3_56p_.go
+++ TRY_3_56p_.go
@@ -1,15 +1,16 @@
 package main
+// 数字を受け取って偶数か奇数を返す関数
+
 
 func main() {
 	for i := 1; 1 <= 100; i++{
 		print(i)
-		if(i%2 == 0 && i != 100) {
-			println("-偶数")
+		if(i%2 == 0 && i != 100) { // ここを関数にして移動するする
+//			println("-偶数")
 		} else if(i%2 == 1) {
 			println("-奇数")
 		} else {
 			println("-偶数")
-			break;
 		}
 	}
 }
