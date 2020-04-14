f36687c56cb1ce805e38a49ad70c1a7904fe72ce
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-14 13:54:21 +0900

    Update TRY_3_56p_.go
    ここもタイポしてるやんけえええええ！！！！！

--- TRY_3_56p_.go
+++ TRY_3_56p_.go
@@ -1,7 +1,19 @@
 package main
 // 数字を受け取って偶数か奇数を返す関数
 
+func main() {
+	for i := 1; i <= 100; i++{
+		print(i)
+		if(i%2 == 1) {
+			println("-奇数")
+		} else {
+			println("-偶数")
+		}
+	}
+}
+	
 
+/*
 func main() {
 	for i := 1; 1 <= 100; i++{
 		print(i)
@@ -14,6 +26,6 @@ func main() {
 		}
 	}
 }
-	
+*/	
 
 // 何で１００まで繰り返さないのかわからないけど、力づくで100でカウントが止まるようにした。
