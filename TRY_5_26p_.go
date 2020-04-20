825ad32a30ea99a22c35a124a7459f95cacafea9
YuiUehara <62981132+YuiUehara@users.noreply.github.com>
2020-04-20 16:05:33 +0900

    Update TRY_5_26p_.go

--- TRY_5_26p_.go
+++ TRY_5_26p_.go
@@ -1,1 +1,29 @@
+package main
 
+// 5章 コマンドラインツール 26p 【TRY】 catコマンドを作ろう
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	// p23より参照
+	// パスを結合する
+	path := filepath.Join("dir", "main.go")
+	fmt.Println(path)
+	// ファイル名を取得
+	fmt.Println(filepath.Base(path))
+
+	// p22 より参照
+	// 標準入力から読み込む
+	scanner := bufio.NewScanner(os.Stdin)
+	// 1行ずつ読み込んで繰り返す
+	for scanner.Scan() {
+		//1行分を出力する
+		fmt.Println(scanner.Text())
+	}
+	fmt.Println()
+}
